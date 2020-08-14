b5f396ca9ba390f38e5c1c3f2b5d0f61d1bec982
Ronivaldo Santos <ronivaldosan@gmail.com>
2020-08-14 12:43:47 -0300

    Simple Create form

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
@@ -17,12 +18,24 @@ namespace SalesWebMvc.Controllers
             _sellerService = sellerService;
         }
 
-
         public IActionResult Index()
         {
             //Retorna para view uma lista dos vendedores.
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost] //Anotacion indicando o tipo do verbo
+        [ValidateAntiForgeryToken] // Evitar ataques de hackers que aproveitam requisão aberta.
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller); //Chama o método para gravar no BD
+            return RedirectToAction(nameof(Index)); //Redireciona para recarregar a página.
         }
     }
 }
--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -17,14 +17,19 @@ namespace SalesWebMvc.Services
         }
 
         //Operação para retornar todos os vendedores cadastrados
-
         public List<Seller> FindAll()
         {
-            return _context.Seller.ToList();
-        
+            return _context.Seller.ToList();        
         }
 
+        //Acão para cadastrar novo vendedor
+        public void Insert(Seller obj)
+        {
+            //Cadastra novo vendedor
+            _context.Add(obj);
 
-
+            //Confirma a atualização no BD
+            _context.SaveChanges();
+        }        
     }
 }
